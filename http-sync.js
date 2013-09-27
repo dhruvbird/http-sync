75913c863c3fcd1479d2734edf6860ac42d3a314
Sean Massa <smassa@groupon.com>
2013-09-27 14:37:13 -0700

    cleaned up whitespace

--- http-sync.js
+++ http-sync.js
@@ -1,83 +1,81 @@
 var _cl = require('./curllib');
 var curllib = new _cl.CurlLib();
 
-// console.log("curlllib:", curllib);
-
 function CurlRequest(options) {
     this._options = options;
     this._headers = { };
 
     var k;
     for (k in this._options.headers) {
-	this.setHeader(k, this._options.headers[k]);
+        this.setHeader(k, this._options.headers[k]);
     }
 }
 
 CurlRequest.prototype = {
     getHeader: function(name) {
-	return this._headers[name.toLowerCase()];
-    }, 
+      return this._headers[name.toLowerCase()];
+    },
     removeHeader: function(name) {
-	delete this._headers[name.toLowerCase()];
-    }, 
+        delete this._headers[name.toLowerCase()];
+    },
     setHeader: function(name, value) {
-	this._headers[name.toLowerCase()] = value;
-    }, 
+        this._headers[name.toLowerCase()] = value;
+    },
     write: function(data) {
-	data = data || '';
-	this._options.body += data;
-    }, 
+        data = data || '';
+        this._options.body += data;
+    },
     end: function(data) {
-	this.write(data);
-	var _ep = this._options.protocol + '://' + this._options.host + 
-	    ':' + this._options.port + this._options.path;
-	var _h = [ ];
-	var k;
-	for (k in this._headers) {
-	    _h.push(k + ': ' + this._headers[k]);
-	}
+        this.write(data);
+        var _ep = this._options.protocol + '://' + this._options.host +
+            ':' + this._options.port + this._options.path;
+        var _h = [ ];
+        var k;
+        for (k in this._headers) {
+            _h.push(k + ': ' + this._headers[k]);
+        }
 
-	var ret = curllib.run(this._options.method, _ep, 
-			      _h, this._options.body);
+        var ret = curllib.run(this._options.method, _ep,
+                      _h, this._options.body);
 
-	if (ret.error) {
-	    throw new Error(ret.error);
-	}
+        if (ret.error) {
+            throw new Error(ret.error);
+        }
 
-	ret.body = '';
-	if (ret.body_length) {
-	    var _b = new Buffer(ret.body_length);
-	    ret.body = curllib.body(_b);
-	}
+        ret.body = '';
+        if (ret.body_length) {
+            var _b = new Buffer(ret.body_length);
+            ret.body = curllib.body(_b);
+        }
 
-	function _parse_headers(headers) {
-	    var _sre = /HTTP\/[0-9].[0-9] ([0-9]{3})/;
-	    var _hre = /([^:]+):([\s]*)([^\r\n]*)/;
-	    var statusCode = 200;
-	    var _h = { };
-	    headers.forEach(function(line) {
-		var _m = line.match(_sre);
-		if (_m) {
-		    statusCode = _m[1];
-		}
-		else {
-		    _m = line.match(_hre);
-		    if (_m) {
-			_h[_m[1]] = _m[3];
-		    }
-		}
-	    });
-	    return {
-		statusCode: statusCode, 
-		headers: _h
-	    };
-	}
+        function _parse_headers(headers) {
+            var _sre = /HTTP\/[0-9].[0-9] ([0-9]{3})/;
+            var _hre = /([^:]+):([\s]*)([^\r\n]*)/;
+            var statusCode = 200;
+            var _h = { };
+            headers.forEach(function(line) {
+                var _m = line.match(_sre);
+                if (_m) {
+                    statusCode = _m[1];
+                }
+                else {
+                    _m = line.match(_hre);
+                    if (_m) {
+                    _h[_m[1]] = _m[3];
+                    }
+                }
+            });
+            return {
+                statusCode: statusCode,
+                headers: _h
+            };
+        }
 
-	var _ph = _parse_headers(ret.headers);
-	ret.statusCode = _ph.statusCode;
-	ret.headers    = _ph.headers;
+        var _ph = _parse_headers(ret.headers);
+        ret.statusCode = _ph.statusCode;
+        ret.headers    = _ph.headers;
 
-	return ret;
+        return ret;
     }
 };
 
--- test_require.js
+++ test_require.js
@@ -7,22 +7,20 @@ function remote_require(_url) {
     var _u = url.parse(_url);
 
     var req = http_sync.request({
-	host: _u.hostname, 
-	port: _u.port, 
-	path: _u.pathname
+        host: _u.hostname,
+        port: _u.port,
+        path: _u.pathname
     });
 
     var res = req.end();
-    // console.log(res);
-    // console.log(res.body.toString());
     var _fname = '/tmp/some_random_name_that_changes_every_time.js';
     fs.writeFileSync(_fname, res.body.toString());
     return require(_fname);
 }
 
 // Fetch underscore.js
-var _ = remote_require('http://documentcloud.github.com/underscore/underscore-min.js');
-// console.log(_);
+var _ = remote_require('http://documentcloud.github.io/underscore/underscore-min.js');
 
 // Do something with _
 console.log(_([1,2,3,4]).map(function(x) { return x*x; }));
+
